2fd6799517cd90229e54c65fa9226b8aa7af89d5
rr <rr@gmail.com>
2023-07-30 17:47:21 +0530

    final cal

--- cal.py
+++ cal.py
@@ -1,11 +1,13 @@
+from add import *
+from sub import *
 n1=int(input("Enter the number1"))
 n2=int(input("Enter the number2"))
 
 
 
-print("Addition is ")
-add(n1,n2)
+print("Addition is ",add(n1,n2))
 
 
-print("Substraction is")
-sub(n1,n2)
+
+print("Substraction is",sub(n1,n2))
+
